6b68fd15d0bccfb50dec205e7183c0fce85260dc
Laura <theimer.laura@gmail.com>
2020-07-08 21:28:17 -0400

    have 13_files fixed, working, 15 has new errors.

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,15 +10,21 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 #!!!!!!!!  
 # YOUR CODE HERE
-a = open("src/foo.txt", "r")
-print(a)
+a = open("src/foo.txt")
+
+for line in a:
+    print(line)
+
 a.close()
+
+## √√ 
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+# # YOUR CODE HERE
 b = open("bar.txt", "w")
 b.write('Hello ')
 b.write(' testing ')
--- src/15_classes.py
+++ src/15_classes.py
@@ -3,39 +3,51 @@
 
 # YOUR CODE HERE
 class LatLon:
-        def init(self, lat, lon):
-            self.lat = lat
-            self.lon = lon
+    def _init_(self, lat=0, lon=0):
+        self.lat = lat
+        self.lon = lon
 
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon. Look up the `super` method.
 
 # YOUR CODE HERE
 class Waypoint(LatLon):
-    def init(self, name, lat, lon):
-        super().init(lat, lon)
+    def _init_(self, name, lat=0, lon=0):
+        super()._init_(lat, lon)
         self.name = name
-    
-    def str(self):
-        return f"<Waypoint '{self.name}', {self. lat}, {self.lon}>"
+
 
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
 
 # YOUR CODE HERE
+class Geocache(Waypoint):
+    def _init_(self, name, difficulty, size, lat=0, lon=0):
+        super()._init_(name, lat, lon)
+        self.difficulty = difficulty
+        self.size = size 
+    def _str_(self):
+        return f"<Geocache '{self.name}', {self.difficulty}, {self.size}, {self.lat},{self.lon} "
 
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
-
 # YOUR CODE HERE
+# w = Waypoint("Catacombs", 41.70505, -121.515221)
+
+'''
+line 37, in <module>
+    w = Waypoint("Catacombs", 41.70505, -121.515221)
+TypeError: Waypoint() takes no arguments
+'''
 
 # Without changing the following line, how can you make it print into something
 # more human-readable? Hint: Look up the `object.__str__` method
-print(waypoint)
+# print(w)
 
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
 
 # YOUR CODE HERE
+g = Geocache("Newberry Views", 1.5, 2, 44.052137, -121.41556)
 
 # Print it--also make this print more nicely
-print(geocache)
+print(g)
